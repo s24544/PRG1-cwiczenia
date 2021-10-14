b6562d6b70c96792489e53848d6bc2038f57974f
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-14 21:45:39 +0200

    zadanie z 08.10.2021

---
+++ 1.0.0.3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+auto ask_user_for_integer(std::string const prompt) -> int
+{
+    if (not prompt.empty()){
+                            std::cout << prompt;
+                            }
+    auto value = std::string{};
+    std::getline(std::cin, value);
+    return std::stoi(value);
+}
+
+auto main() -> int{
+    auto const a = ask_user_for_integer("a = ");
+    auto const b = ask_user_for_integer("b = ");
+    std::cout << (a * b) << "\n";
+    return 0;
+}

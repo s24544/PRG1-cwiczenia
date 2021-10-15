ed24335fcfcf0876fcbeb6c4b036a88a3e38d9b9
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-15 10:17:30 +0200

    zadanie z 15.10.2021

---
+++ s03-fizzbuzz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+auto main(int argc, char *argv[]) -> int{
+int liczba = atoi(argv[1]);
+if(argc !=2){
+std::cout << "Nieprawidłowe argumenty\n";
+return 1;
+}
+else{
+for(int i = 1;i <= liczba; i++){
+if(i % 3 == 0 && i % 5 == 0){
+std::cout << "FizzBuzz\n";
+}
+else if(i % 5 == 0){
+std::cout << "Buzz\n";
+}
+else if(i % 3 == 0){
+std::cout << "Fizz\n";
+}
+else{
+std::cout << i << "\n";
+}
+}
+
+return 0;
+}
+}

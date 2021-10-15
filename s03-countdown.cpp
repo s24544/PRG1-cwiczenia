62c8f01b39c3180262179f099c43f54423ae8239
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-15 09:38:18 +0200

    zadania z 15.10.2021

---
+++ s03-countdown.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int{
+int a = atoi(argv[1]);
+if (argc !=2){
+std::cout << "nieprawidłowe dane\n";
+return 1;
+}
+else{
+if (a > 0){
+for(int i = a; i >= 0; i--){
+std::cout << i << "\n";
+}
+}
+else{
+for(int i = a; i <= 0; i++){
+std::cout << i << "\n";
+}
+}
+
+return 0;
+}
+}

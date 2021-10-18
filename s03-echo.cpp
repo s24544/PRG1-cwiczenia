061835156ff65da5ff398a973f2476dfc9083cd5
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-19 00:34:35 +0200

    zadanie z 15.10.2021

--- s03-echo.cpp
+++ s03-echo.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include <string>
-//bez ćwiczeń dodatkowych
-auto main(int argc, char* argv[]) -> int{
+
+//z "ćwiczeniami dodatkowymi"
+auto main(int argc, char *argv[]) -> int{
 if(argc < 2){
-std::cout << "Za mała liczba argumentów\n";
+std::cout << "Nieprawidłowa ilość argumentów\n";
 return 1;
 }
 else{
-for(int i = 1;i < argc+1;i++){
+if(std::string{argv[1]} == "-n"){ // -n
+for(int i = 2;i < argc;i++){
+std::cout << argv[i] << " ";
+}
+return 0;
+}
+else if(std::string{argv[1]} == "-r"){ //-r
+if(std::string{argv[2]} == "-l"){ // -r -l
+for(int i = argc - 1;i > 2;i--){
+std::cout << argv[i] << "\n";
+}
+std::cout << "\n";
+return 0;
+}
+else if(std::string{argv[2]} == "-n"){ //-r -n
+for(int i = argc - 1;i > 2;i--){
+std::cout << argv[i] << " ";
+}
+return 0;
+}
+for(int i = argc - 1;i > 1;i--){
 std::cout << argv[i] << " ";
 }
 std::cout << "\n";
 return 0;
+}
+else if(std::string{argv[1]} == "-l"){ //-l
+for(int i = 2;i < argc;i++){
+std::cout << argv[i] << "\n";
+}
+std::cout << "\n";
+return 0;
+}
+else{
+for(int i = 1;i < argc;i++){
+std::cout << argv[i] << " ";
+}
+std::cout << "\n";
+return 0;
+}
 }
 }

a24e9dceed70a541ffb3f1f45c58264d37e6452e
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 20:27:45 +0200

    poprawa czytelności

--- 15.10.2021/s03-countdown.cpp
+++ 15.10.2021/s03-countdown.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
-auto main(int argc, char *argv[]) -> int{
-int a = atoi(argv[1]);
-if (argc !=2){
-std::cout << "nieprawidłowe dane\n";
-return 1;
-}
-else{
-if (a > 0){
-for(int i = a; i >= 0; i--){
-std::cout << i << "\n";
-}
-}
-else{
-for(int i = a; i <= 0; i++){
-std::cout << i << "\n";
-}
-}
-
-return 0;
-}
+auto main(int argc, char *argv[]) -> int
+{
+	int a = atoi(argv[1]);
+	if (argc !=2)
+	{
+		std::cout << "nieprawidłowe dane\n";
+		return 1;
+	}
+	else
+	{
+		if (a > 0)
+		{
+			for(int i = a; i >= 0; i--)
+			{
+				std::cout << i << "\n";
+			}
+		}
+		else
+		{
+			for(int i = a; i <= 0; i++)
+			{
+				std::cout << i << "\n";
+			}
+		}
+		return 0;
+	}
 }

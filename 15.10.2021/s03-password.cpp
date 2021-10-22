329e532c4e70f36dd7abf2ab40480d2a5f1b079b
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 20:43:05 +0200

    poprawa czytelności

--- 15.10.2021/s03-password.cpp
+++ 15.10.2021/s03-password.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string>
 
-auto main (int argc, char *argv[]) -> int{
-auto password = std::string{argv[1]};
-auto podaj = std::string{};
-if(argc != 2){
-std::cout << "Nieprawidłowa ilość argumentów!";
-return 1;
-}
-else
+auto main (int argc, char *argv[]) -> int
 {
-do{
-std::cout << "Podaj hasło: ";
-std::cin >> podaj;
-std::cout << "\n";
-}while(password.compare(podaj) != 0);
-std::cout << "ok!\n";
-return 0;
-}
+	auto password = std::string{argv[1]};
+	auto podaj = std::string{};
+	if(argc != 2)
+	{
+		std::cout << "Nieprawidłowa ilość argumentów!";
+		return 1;
+	}
+	else
+	{
+		do
+		{
+			std::cout << "Podaj hasło: ";
+			std::cin >> podaj;
+			std::cout << "\n";
+		}while(password.compare(podaj) != 0);
+		std::cout << "ok!\n";
+		return 0;
+	}
 }

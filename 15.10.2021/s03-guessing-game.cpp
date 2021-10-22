d15cc1cfc6360a27788d976738b01f448a5d45fd
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 20:41:01 +0200

    poprawa ifelse i czytelności

--- 15.10.2021/s03-guessing-game.cpp
+++ 15.10.2021/s03-guessing-game.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <random>
-auto main() -> int{
+
+auto main() -> int
+{
 std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<> distr(1, 100);
-
-int los = distr(gen);
+std::uniform_int_distribution<int> losowanie(1, 100);
+int los = losowanie(rd);
 int a;
-do{
-std::cout << "guess: ";
-std::cin >> a;
-if(a == los){
-std::cout << "just right!\n";
-}
-if(a > los){
-std::cout << "too big!\n";
-}
-if(a < los){
-std::cout << "too small!\n";
-}
 
+do
+{
+	std::cout << "guess: ";
+	std::cin >> a;
+	if(a == los)
+	{
+		std::cout << "just right!\n";
+	}
+	else
+	{
+		if(a > los)
+		{
+			std::cout << "too big!\n";
+		}
+		else
+		{
+			std::cout << "too small!\n";
+		}
 }
-while(a != los);
-
-
+}while(a != los);
 return 0;
 }

5b1b1978ed60530fae70029b2c80944dff29e034
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 18:45:31 +0200

    nagłówkowy do s03-Student.cpp

--- 22.10.2021/include/s03-Student.h
+++ 22.10.2021/include/s03-Student.h
@@ -10,3 +10,13 @@ struct Student {
 	Student() = default;
 	std::string to_string() const;
 };
+
+std::string Student::to_string() const{
+	std::string imie = this -> imie;
+	std::string nazwisko = this -> nazwisko;
+	std::string indeks = this->numer_indeksu;
+	std::string semestr = std::to_string(this->aktualny_semestr);
+	std::string srednia = std::to_string(this->srednia_ocen);
+
+return "Imię i nazwisko: " + imie + " " + nazwisko + "\nIndeks: s" + indeks + "\nSemestr: " + semestr + "\nŚrednia: " + srednia;
+}

8cf74c3da2065e4a52f0df91a6e7d2af33ad6c43
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 21:03:54 +0200

    Update s03-Student.cpp

--- 22.10.2021/s03-Student.cpp
+++ 22.10.2021/s03-Student.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "include/s03-Student.h"
 
-int main(){
-Student ja = Student{"Jan", "Kreft", "24544", 1, 3.5};
-std::cout << ja.to_string() << "\n";
-return 0;
+int main()
+{
+  Student ja = Student{"Jan", "Kreft", "24544", 1, 3.5};
+  std::cout << ja.to_string() << "\n";
+  return 0;
 }

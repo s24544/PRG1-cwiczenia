6538f4659417e1925917926ba54825d4f009efc2
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 20:34:33 +0200

    poprawa ifelse i czytelności

--- 15.10.2021/s03-fizzbuzz.cpp
+++ 15.10.2021/s03-fizzbuzz.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 
-auto main(int argc, char *argv[]) -> int{
-int liczba = atoi(argv[1]);
-if(argc !=2){
-std::cout << "Nieprawidłowe argumenty\n";
-return 1;
-}
-else{
-for(int i = 1;i <= liczba; i++){
-if(i % 3 == 0 && i % 5 == 0){
-std::cout << "FizzBuzz\n";
-}
-else if(i % 5 == 0){
-std::cout << "Buzz\n";
-}
-else if(i % 3 == 0){
-std::cout << "Fizz\n";
-}
-else{
-std::cout << i << "\n";
-}
-}
-return 0;
-}
+auto main(int argc, char *argv[]) -> int
+{
+	int liczba = atoi(argv[1]);
+	if(argc !=2)
+	{
+		std::cout << "Nieprawidłowe argumenty\n";
+		return 1;
+	}
+	else
+	{
+	for(int i = 1;i <= liczba; i++)
+	{
+		if(i % 3 == 0 && i % 5 == 0)
+		{
+			std::cout << "FizzBuzz\n";
+		}
+		else
+		{
+			if(i % 5 == 0)
+			{
+				std::cout << "Buzz\n";
+			}
+			else
+			{
+				if(i % 3 == 0)
+				{
+					std::cout << "Fizz\n";
+				}
+				else
+				{
+					std::cout << i << "\n";
+				}
+			}
+		}
+	}
+		return 0;
+	}
 }

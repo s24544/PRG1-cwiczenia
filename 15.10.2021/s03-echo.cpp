fe02ddbedf9a0c3a7a31c55ba90eb481c3008ac9
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-22 20:07:08 +0200

    poprawa czytelności

--- 15.10.2021/s03-echo.cpp
+++ 15.10.2021/s03-echo.cpp
@@ -1,51 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-auto main(int argc, char *argv[]) -> int{
-if(argc < 2){
-std::cout << "Nieprawidłowa ilość argumentów\n";
-return 1;
+int main(int argc, char *argv[]){
+if(argc < 2)
+{
+	std::cout << "Nieprawidłowa ilość argumentów\n";
+	return 1;
 }
-else{
-if(std::string{argv[1]} == "-n"){ // -n
-for(int i = 2;i < argc;i++){
-std::cout << argv[i] << " ";
+else
+{
+	std::string flaga_n = "\n";
+	std::string flaga_l = "";
+	auto args = std::vector<std::string>{};
+	std::copy_n(argv, argc, std::back_inserter(args));
+	args.erase(args.begin());
+	//3 ify zamiast if else bo mogą być spełnione 2ify jednocześnie w obojętnej kolejności
+	if(args[0] == "-n" || args[1] == "-n")
+	{
+		flaga_n = "";
+		args.erase(std::remove(args.begin(), args.end(), "-n"));
+	}
+	if(args[0] == "-l"  || args[1] == "-l")
+	{
+		args.erase(std::remove(args.begin(), args.end(), "-l"));
+		flaga_l = "\n";
+	}
+	if(args[0] == "-r" || args[1] == "-r")
+	{
+		args.erase(std::remove(args.begin(), args.end(), "-r"));
+		std::reverse(args.begin(), args.end());
+	}
+
+	for (auto const& each: args)
+	{
+		std::cout << each << " " << flaga_l;
+	}
+std::cout << flaga_n;
 }
 return 0;
-}
-else if(std::string{argv[1]} == "-r"){ //-r
-if(std::string{argv[2]} == "-l"){ // -r -l
-for(int i = argc - 1;i > 2;i--){
-std::cout << argv[i] << "\n";
-}
-std::cout << "\n";
-return 0;
-}
-else if(std::string{argv[2]} == "-n"){ //-r -n
-for(int i = argc - 1;i > 2;i--){
-std::cout << argv[i] << " ";
-}
-return 0;
-}
-for(int i = argc - 1;i > 1;i--){
-std::cout << argv[i] << " ";
-}
-std::cout << "\n";
-return 0;
-}
-else if(std::string{argv[1]} == "-l"){ //-l
-for(int i = 2;i < argc;i++){
-std::cout << argv[i] << "\n";
-}
-std::cout << "\n";
-return 0;
-}
-else{
-for(int i = 1;i < argc;i++){
-std::cout << argv[i] << " ";
-}
-std::cout << "\n";
-return 0;
-}
-}
 }

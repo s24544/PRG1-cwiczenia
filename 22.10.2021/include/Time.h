7ef0c71e5673a0b6a65bab50bd1c99876ddfd24c
s24544 <61560856+s24544@users.noreply.github.com>
2021-10-29 08:55:14 +0200

    Update Time.h

--- 22.10.2021/include/Time.h
+++ 22.10.2021/include/Time.h
@@ -1,13 +1,14 @@
 #include <string>
 
 struct Time {
-	int godzina;
-	int minuta;
-	int sekunda;
+	int hour;
+	int minute;
+	int second;
 
 	Time() = default;
 	std::string to_string() const;
 	void next_hour();
 	void next_minute();
 	void next_second();
+	void time_verify();
 };

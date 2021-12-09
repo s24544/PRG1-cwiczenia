988e3165f86b473aba7043f22a6bdd23882b3e62
s24544 <61560856+s24544@users.noreply.github.com>
2021-12-09 15:15:09 +0100

    Update 3.2.0.7.cpp

--- 19.11.2021/3.2.0.7.cpp
+++ 19.11.2021/3.2.0.7.cpp
@@ -4,26 +4,25 @@ void mem_rev(void* s, size_t n)
 {
     char* a = (char*) s;
     char* b = new char[n];
-    for(size_t i = 0;i<n; i++)
+    for(size_t i = 0, j=n-1;i<n; i++)
     {
-        *(b+i) = *(a+n-1);
+        *(b+i) = *(a+j);
+        j = j - 1;
     }
     for(size_t i = 0;i < n;i++)
     {
         *(a+i) = *(b+i);
     }
 }
-int main()
-{
+
+int main() {
     char a[] = "Hello, World!";
     for(char i : a)
-    {
-        std::cout << i;
-    }
+        std::cout << i; //print przed
+    std::cout << "\n";
     mem_rev(a, sizeof(a));
     for(char i : a)
-    {
-        std::cout << i;
-    }
+        std::cout << i; //print po uzyciu funkcji
+
     return 0;
 }

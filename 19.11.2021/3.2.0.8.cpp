52410d4d7356c698c7ab5337760b318ff875a22b
s24544 <61560856+s24544@users.noreply.github.com>
2021-12-09 22:46:04 +0100

    Update 3.2.0.8.cpp

--- 19.11.2021/3.2.0.8.cpp
+++ 19.11.2021/3.2.0.8.cpp
@@ -3,25 +3,19 @@
 
 void mem_rand(void* s, size_t n)
 {
-	char* s_wsk = (char*)s;
-	std::random_device rd;
-	std::uniform_int_distribution<int> rand_byte(0, 255);
-	for(size_t i = 0; i < n; i++)
-	{
-		*(s_wsk+i) = rand_byte(rd);
-	}
+    char* s_wsk = (char*)s;
+    std::random_device rd;
+    std::uniform_int_distribution<int> rand_byte(0, 8);
+    for (size_t i = 0; i < n; i++)
+        *(s_wsk + i) = rand_byte(rd);
 }
 
 
 int main()
 {
-	char a[255];
-	int n = sizeof(a);
-	mem_rand(a, n);
-	for(int i = 0; i < n; i++)
-	{
-		std::cout << a[i] << " ";
-	}
-	std::cout << "\n";
-	return 0;
+    char a[8];
+    mem_rand(a, sizeof(a));
+    for(char i : a)
+        std::cout << i;
+    return 0;
 }

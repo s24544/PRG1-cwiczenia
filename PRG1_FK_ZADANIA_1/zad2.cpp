8595537b3cb4868967e172149425c3a54e75ef65
s24544 <61560856+s24544@users.noreply.github.com>
2021-12-10 08:40:20 +0100

    Create zad2.cpp

---
+++ PRG1_FK_ZADANIA_1/zad2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string imie = "Jan";
+    double waga = 75.53;
+    int wzrost = 184;
+    std::cout << "Imie: " << imie << std::endl;
+    std::cout << "\n";
+    std::cout << "Waga: " << waga << " kg"<< std::endl;
+    std::cout << "\n";
+    std::cout << "Wzrost: " << wzrost << " cm";
+}
